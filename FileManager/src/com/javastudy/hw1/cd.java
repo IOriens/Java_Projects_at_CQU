05b56a545d2a293faf229656b706980129e9bb84
IOriens <812561857@qq.com>
2015-10-18 21:27:29 +0800

    添加注释

--- FileManager/src/com/javastudy/hw1/cd.java
+++ FileManager/src/com/javastudy/hw1/cd.java
@@ -6,8 +6,10 @@ public class cd implements Manager {
 
 	@Override
 	public void excute(String[] args, StringBuffer currDir) {
-		// TODO Auto-generated method stub
-		if("..".equals(args[1])){//输入cd ..进入上层目录			
+
+		if("..".equals(args[1])){
+			
+			/*输入cd ..进入上层目录*/						
 			String[] tmp=currDir.toString().split("\\\\");//使用正则表达式来分割地址
 			StringBuffer tmp2=new StringBuffer(tmp[0]);			
 			for(int i=1;i<tmp.length-1;i++){					
@@ -15,16 +17,22 @@ public class cd implements Manager {
 				tmp2.append(tmp[i]);				
 			}//剔除最后一层目录
 			
-			//重点 清除传入的StringBuffer的内容，并修改为替代内容
+			//重点：清除传入的StringBuffer的内容，并修改为替代内容
 			currDir.replace(0, currDir.length(), tmp2.toString());
+			
 		}else if(args[1].contains(":\\") || args[1].charAt(0) == '/') {
-			//StringBuffer tempForCheck=;
+			
+			/*包含绝对路径的情况*/
 			File tempFileDir=new File(args[1]);
+			//重点：需要判断路径是否存在
 			if(tempFileDir.isDirectory())
 				currDir=new StringBuffer(args[1]);
 			else
 				System.out.println("该目录不存在");
+			
 		}else{
+			
+			/*使用相对路径的情况*/
 			StringBuffer tempForCheck=new StringBuffer(currDir);
 			tempForCheck.append("\\"+args[1]);
 			File tempFileDir=new File(tempForCheck.toString());
