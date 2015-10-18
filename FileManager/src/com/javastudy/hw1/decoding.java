9508255362e24c52b0f22cdcd3a65ee1a6d23d59
IOriens <812561857@qq.com>
2015-10-18 19:31:37 +0800

    decoding未解决

--- FileManager/src/com/javastudy/hw1/decoding.java
+++ FileManager/src/com/javastudy/hw1/decoding.java
@@ -1,11 +1,114 @@
 package com.javastudy.hw1;
 
-public class decoding implements Manager{
+import java.io.*;
+
+
+public class decoding implements Manager {
 
 	@Override
 	public void excute(String[] args, StringBuffer currDir) {
 		// TODO Auto-generated method stub
-		System.out.println("文件解密");
+		FileInputStream g = null;
+		FileInputStream k1 = null;
+		String msg1 = "";
+		String decmsg = "";
+		String key = "";
+		int i = 0;
+		if (args[3].contains(":\\")) {
+			try {
+				g = new FileInputStream(args[3]);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			StringBuffer tempdir = new StringBuffer(currDir);
+			tempdir.append("\\" + args[3]);
+			try {
+				g = new FileInputStream(tempdir.toString());
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		do {
+			try {
+				i = g.read();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (i != -1) {
+				msg1 = msg1 + (char) i;
+				System.out.print((char) i);
+			}
+
+		} while (i != -1);
+		try {
+			g.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		System.out.println("");
+		System.out.println("plese input  key");
+		String key1 = null;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			key1 = br.readLine();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		if (args[2].contains(":\\")) {
+			try {
+				k1 = new FileInputStream(args[2]);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			StringBuffer tempdir = new StringBuffer(currDir);
+			tempdir.append("\\" + args[2]);
+			try {
+				k1 = new FileInputStream(tempdir.toString());
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		do {
+			try {
+				i = k1.read();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (i != -1)
+				key = key + (char) i;
+		} while (i != -1);
+		if (key.length() == key1.length()) {
+			int l = 0;
+			for (int n = 0; n < key.length(); n++) {
+				if (key.charAt(n) == key1.charAt(n)) {
+					if (n == key.length() - 1)
+						l = 1;
+					continue;
+				} else
+					System.out.println("error key");
+			}
+			if (l == 1) {
+				int q = 0;
+				for (int z = 0; z < msg1.length(); z++) {
+					decmsg = decmsg + (char) (msg1.charAt(z) ^ key1.charAt(q));
+					q++;
+					if (q == key.length())
+						q = 0;
+				}
+				System.out.println(decmsg);
+			}
+		} else
+			System.out.println("error key");
 	}
-	
+
 }
--- FileManager/src/com/javastudy/hw1/encoding.java
+++ FileManager/src/com/javastudy/hw1/encoding.java
@@ -1,10 +1,131 @@
 package com.javastudy.hw1;
 
-public class encoding implements Manager{
+import java.io.*;
+//import java.util.Scanner;
+
+public class encoding implements Manager {
 
 	@Override
 	public void excute(String[] args, StringBuffer currDir) {
 		// TODO Auto-generated method stub
-		System.out.println("文件加密");
+		FileInputStream f = null;
+		if (args[1].contains(":\\")) {
+			try {
+				f = new FileInputStream(args[1]);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			StringBuffer tempdir = new StringBuffer(currDir);
+			tempdir.append("\\" + args[1]);
+			try {
+				f = new FileInputStream(tempdir.toString());
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		int q = 0;
+		int i = 0;
+		String encmsg = "";
+		String msg = "";
+		do {
+			try {
+				i = f.read();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (i != -1) {
+				msg = msg + (char) i;
+				System.out.print((char) i);
+			}
+		} while (i != -1);
+		System.out.println("");
+		System.out.println("plese input  key");
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String key = null;
+		try {
+			key = br.readLine();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		FileOutputStream k = null;
+		if (args[2].contains(":\\")) {
+			try {
+				k = new FileOutputStream(args[2]);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			StringBuffer tempdir2 = new StringBuffer(currDir);
+			tempdir2.append("\\" + args[2]);
+			try {
+				k = new FileOutputStream(tempdir2.toString());
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		for (int p = 0; p < key.length(); p++)
+			try {
+				k.write(key.charAt(p));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		try {
+			k.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		for (int t = 0; t < msg.length(); t++) {
+			encmsg = encmsg + (char) (msg.charAt(t) ^ key.charAt(q));
+			q++;
+			if (q == key.length())
+				q = 0;
+		}
+		try {
+			f.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		FileOutputStream fout = null;
+		if (args[3].contains(":\\")) {
+			try {
+				fout = new FileOutputStream(args[3]);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			StringBuffer tempdir = new StringBuffer(currDir);
+			tempdir.append("\\" + args[3]);
+			try {
+				fout = new FileOutputStream(tempdir.toString());
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		for (int p = 0; p < encmsg.length(); p++)
+			try {
+				fout.write(encmsg.charAt(p));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		try {
+			fout.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 }
