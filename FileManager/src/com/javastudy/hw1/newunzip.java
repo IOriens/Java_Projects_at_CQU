0a347ac3dfb19d1065d979969a5b26199a7c20bf
IOriens <812561857@qq.com>
2015-10-30 22:21:38 +0800

    merge from zhaoyang

--- FileManager/src/com/javastudy/hw1/newunzip.java
+++ FileManager/src/com/javastudy/hw1/newunzip.java
@@ -1,10 +1,40 @@
 package com.javastudy.hw1;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 public class newunzip implements Manager{
 
 	@Override
 	public void excute(String[] args, StringBuffer currDir) {
 		// TODO Auto-generated method stub
 		System.out.println("文件解压缩");
-	}	
+		try {
+			// Open the ZIP file
+			@SuppressWarnings("resource")
+			ZipFile zf = new ZipFile(args[1]);
+			for (Enumeration entries = zf.entries(); entries.hasMoreElements(); ) {
+				// Get the entry name
+				ZipEntry entry = ( (ZipEntry) entries.nextElement());
+				String zipEntryName = entry.getName();
+				java.io.InputStream in = zf.getInputStream(entry);
+				// System.out.println(zipEntryName);
+				FileOutputStream out = new FileOutputStream(args[1] + zipEntryName);
+				byte[] buf1 = new byte[1024];
+				int len;
+				while ( (len = in.read(buf1)) > 0) {
+					out.write(buf1, 0, len);
+				}
+				// Close the file and stream
+				in.close();
+				out.close();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- FileManager/src/com/javastudy/hw1/newzip.java
+++ FileManager/src/com/javastudy/hw1/newzip.java
@@ -1,11 +1,42 @@
 package com.javastudy.hw1;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 public class newzip implements Manager{
 
 	@Override
 	public void excute(String[] args, StringBuffer currDir) {
 		// TODO Auto-generated method stub
 		System.out.println("文件压缩");
-	}
 
+		byte[] buf = new byte[1024];
+		try {
+			// Create the ZIP file
+			ZipOutputStream out = new ZipOutputStream(new FileOutputStream(args[2]));
+			// Compress the files
+			FileInputStream in = new FileInputStream(args[1]);
+			// Add ZIP entry to output stream.
+			out.putNextEntry(new ZipEntry(args[1]));
+			// Transfer bytes from the file to the ZIP file
+			int len;
+			while ( (len = in.read(buf)) > 0) {
+				out.write(buf, 0, len);
+			}
+			// Complete the entry
+			out.closeEntry();
+			in.close();
+
+			// Complete the ZIP file
+			out.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
+
+
